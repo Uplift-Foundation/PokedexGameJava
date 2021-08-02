446d5d19d007261149127ac3382ee1bded442de7
lunarjuice <87146086+lunarjuice@users.noreply.github.com>
2021-08-02 15:19:01 -0500

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        PokedexItem meow = new PokedexItem("red");
+        System.out.println(meow.getColor());
+    }
+}
