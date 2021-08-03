8e07ce65d1500632987313f2136420145a7aae14
Cristian Martinez <dbautista2681@gmail.com>
2021-08-03 10:04:58 -0500

    Added in states, Only Behaviours left.

--- src/Plant.java
+++ src/Plant.java
@@ -1,3 +1,53 @@
 public class Plant {
+
+    private String smell;
+
+    private Boolean hasLeaves;
+
+    private Boolean hasRoots;
+
+    private Boolean collectsSunLight;
+
+    private Boolean makesGlucose;
+    
+
+    public Plant(String smell, Boolean hasLeaves, Boolean hasRoots, Boolean collectsSunLight, Boolean makesGlucose) { //capital took off some of the sqiggles? 
+        this.setSmell(smell);
+        this.setHasLeaves(hasLeaves);
+        this.setHasRoots(hasRoots);
+        this.setCollectsSunLight(collectsSunLight);
+        this.setMakesGlucose(makesGlucose);
+    }
+    public String getSmell(){
+        return smell; 
+    }
+    public Boolean getHasLeaves(){
+        return hasLeaves;
+    }
+    public Boolean getHasRoots(){
+        return hasRoots; 
+    }
+    public Boolean getCollectsSunLight(){
+        return collectsSunLight;
+    }
+    public Boolean getMakesGlucose(){
+        return makesGlucose;
+    }
+    public void setSmell(String smell){
+        this.smell = smell;
+    }
+    public void setHasLeaves(Boolean hasLeaves){
+        this.hasLeaves = hasLeaves; 
+    }
+    public void setHasRoots(Boolean hasRoots){
+        this.hasRoots = hasRoots; 
+    }
+    public void setCollectsSunLight(Boolean collectsSunLight){
+        this.collectsSunLight = collectsSunLight;
+    }
+    public void setMakesGlucose(Boolean makesGluecose){
+        this.makesGlucose = makesGluecose;
+    }
+    
     
 }
