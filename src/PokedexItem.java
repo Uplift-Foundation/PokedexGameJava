e22c55ff1dfcbefde48c2ec595c5693408f6ea9a
Cristian Martinez <dbautista2681@gmail.com>
2021-08-03 13:36:19 -0500

    Changed private to protected String

--- src/PokedexItem.java
+++ src/PokedexItem.java
@@ -1,18 +1,18 @@
 public class PokedexItem {
     //states
-      private String color;
-      //  public String Color { get => color; set => color = value; }
+    protected String color;
+    //  public String Color { get => color; set => color = value; }
     
-        public PokedexItem(String color)
-        {
-            this.setColor(color);
-        }
-
-        public String getColor() {
-            return color;
-        }
-
-        public void setColor(String color) {
-            this.color = color;
-        }
+    public PokedexItem(String color)
+    {
+        this.setColor(color);
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
