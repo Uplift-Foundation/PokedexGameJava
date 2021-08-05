a8848898f516dffea5b8b3490207e3f73fa6fc22
Rewpi <83379309+Rewpi@users.noreply.github.com>
2021-08-05 13:51:41 -0500

    Challenger Pokedex Update

--- src/ChallengerPokedex.java
+++ src/ChallengerPokedex.java
@@ -1,22 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ChallengerPokedex {
-    Boolean screenIsLitUp2;
+public class ChallengerPokedex extends Pokedex {
     List<ActualPokemon> pokemons = new ArrayList<ActualPokemon>();
-    
-    public ChallengerPokedex(Boolean screenIsLitUp2){
-       this.screenIsLitUp2 = screenIsLitUp2;
-    }
-    public Boolean getScreenIsLitUp2(){
-        return this.screenIsLitUp2;
+    public ChallengerPokedex(boolean screenIsLitUp) {
+        super(screenIsLitUp);
     }
 
-    public List<ActualPokemon> getPokemons() {
-        return pokemons;
-    }
-
-    public void setPokemons(List<ActualPokemon> pokemons) {
-        this.pokemons = pokemons;
-    }
 }
