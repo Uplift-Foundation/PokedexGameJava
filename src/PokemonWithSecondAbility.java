66955306a0e396540006311806284a3ed39b8a0a
Cristian Martinez <dbautista2681@gmail.com>
2021-08-05 22:16:37 -0500

    Added String ability2 to accesor.

--- src/PokemonWithSecondAbility.java
+++ src/PokemonWithSecondAbility.java
@@ -4,7 +4,7 @@ public class PokemonWithSecondAbility extends Pokemon {
 
     public PokemonWithSecondAbility(String name, String avgSize, String color, String pokemonType, String region,
             Integer hp, Integer attack, Integer defense, Integer spDefense, Integer spAttack, Integer speed,
-            String ability1, String typeEffectiveness, String typeWeakness) {
+            String ability1,String ability2, String typeEffectiveness, String typeWeakness) {
         super(name, avgSize, color, pokemonType, region, hp, attack, defense, spDefense, spAttack, speed, ability1,
                 typeEffectiveness, typeWeakness);
     } 
