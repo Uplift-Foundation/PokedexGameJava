4557ad628842a9865f8979740d8d5a17be099921
Cristian Martinez <dbautista2681@gmail.com>
2021-08-09 22:18:13 -0500

    Testing making the Plant object.

---
+++ src/test/java/com/senecafoundation/PlantTest.java
@@ -0,0 +1,35 @@
+package com.senecafoundation;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PlantTest {
+
+    private Plant systemUnderTest;
+
+    @BeforeEach
+    void assignPlant(){
+        this.systemUnderTest = new Plant(
+            "green",
+            "fresh",
+            true,
+            true,
+            true,
+            true
+            );
+    }
+
+    @Test 
+    void testPlantCreate(){
+        assertEquals("green", this.systemUnderTest.getColor());
+        assertEquals("fresh", this.systemUnderTest.getSmell());
+        assertEquals(true, this.systemUnderTest.getHasLeaves());
+        assertEquals(true, this.systemUnderTest.getHasRoots());
+        assertEquals(true, this.systemUnderTest.getCollectsSunLight());
+        assertEquals(true, this.systemUnderTest.getMakesGlucose());
+    }
+
+    
+    
+}
