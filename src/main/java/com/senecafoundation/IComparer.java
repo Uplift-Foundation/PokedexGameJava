7c5e0d3f7840225128e20f988e9db4673d4d93a6
Cristian Martinez <dbautista2681@gmail.com>
2021-08-11 17:36:25 -0500

    Working on making the Interface for Comparer.

--- src/main/java/com/senecafoundation/IComparer.java
+++ src/main/java/com/senecafoundation/IComparer.java
@@ -2,4 +2,12 @@ package com.senecafoundation;
 
 public class IComparer {
     
+    private pokemon1();
+    private pokemon2();
+    
+    
+        public IComparer(PokemonWithSecondAbility pokemonfromuser1, PokemonWithSecondAbility pokemonfromuser2) {
+            this.pokemon1 = pokemonfromuser1;
+            this.pokemon2 = pokemonfromuser2
+        }
 }
