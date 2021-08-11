e05403d27c8d6a0d3229badd2242fa0bea397530
lunarjuice <87146086+lunarjuice@users.noreply.github.com>
2021-08-10 23:14:15 -0500

    Completed Pokemon Test

--- src/test/java/com/senecafoundation/PokemonTest.java
+++ src/test/java/com/senecafoundation/PokemonTest.java
@@ -1,5 +1,48 @@
 package com.senecafoundation;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
 public class PokemonTest {
     
+    private Pokemon systemUnderTest;  
+
+    @BeforeEach
+    void assignPokemon() {
+        this.systemUnderTest = new Pokemon(
+        "meow",
+        "small",
+        "yellow",
+        "cat",
+        "Kanto",
+        40,
+        41,
+        42,
+        43,
+        44,
+        45,
+        "big meow",
+        "kat",
+        "kitty"
+        );
+    }
+
+    @Test
+    void testPokemonCreate() {
+        assertEquals("meow", this.systemUnderTest.getName());
+        assertEquals("small", this.systemUnderTest.getAvgSize());
+        assertEquals("yellow", this.systemUnderTest.getColor());
+        assertEquals("cat", this.systemUnderTest.getPokemonType());
+        assertEquals("Kanto", this.systemUnderTest.getRegion());
+        assertEquals(40, this.systemUnderTest.getHp());
+        assertEquals(41, this.systemUnderTest.getAttack());
+        assertEquals(42, this.systemUnderTest.getDefense());
+        assertEquals(43, this.systemUnderTest.getSpDefense());
+        assertEquals(44, this.systemUnderTest.getSpAttack());
+        assertEquals(45, this.systemUnderTest.getSpeed());
+        assertEquals("big meow", this.systemUnderTest.getAbility1());
+        assertEquals("kat", this.systemUnderTest.getTypeEffectiveness());
+        assertEquals("kitty", this.systemUnderTest.getTypeWeakness());
+    }
 }
