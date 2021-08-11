54ffe004a99f4dd1ed542f105d697f5a4f699864
Cristian Martinez <dbautista2681@gmail.com>
2021-08-11 17:58:10 -0500

    Changed Comparer back to interface file

--- src/main/java/com/senecafoundation/IComparer.java
+++ src/main/java/com/senecafoundation/IComparer.java
@@ -1,13 +1,5 @@
 package com.senecafoundation;
 
-public class IComparer {
+public interface IComparer {
     
-    private pokemon1();
-    private pokemon2();
-    
-    
-        public IComparer(PokemonWithSecondAbility pokemonfromuser1, PokemonWithSecondAbility pokemonfromuser2) {
-            this.pokemon1 = pokemonfromuser1;
-            this.pokemon2 = pokemonfromuser2
-        }
 }
